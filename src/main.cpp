88ad74222ba8933a9ae81d9fe9854dab9d87703c
Dino Leung <dino.ph.leung@gmail.com>
2026-03-16 20:41:34 +1030

    setting up racechrono ble

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 #include "can_bus.h"
+#include "racechrono.h"
 #include "sensor_tasks.h"
 #include "can_tasks.h"
+#include "racechrono_tasks.h"
 
 void setup() {
 	Serial.begin(115200);
 
-	initCan1();
-	initCan2();
+	// initCan1();
+	// initCan2();
+	initRaceChronoBle();
 	
-	startCanTasks();
-	startSensorTasks();
+	// startCanTasks();
+	// startSensorTasks();
+	startRaceChronoTasks();
 }
 
 void loop() { /* DO NOTHING */ }
